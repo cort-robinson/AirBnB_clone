42ed70c2f2a12deb0b1e8251c8b0f02b3fde0b79
dreeseh <1859@holbertonschool.com>
2020-11-05 19:54:50 +0000

    update to unittests for task 5

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,6 +2,15 @@
 """unittest for class FileStorage"""
 import unittest
 import pep8
+import os
+
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from datetime import datetime
@@ -17,6 +26,35 @@ class TestFileStorage(unittest.TestCase):
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
+    def test_file_path(self):
+        """tests the file path is functional"""
+        self.basemodel = BaseModel()
+        self.basemodel.save()
+        self.assertTrue(os.path.isfile("./file.json"))
+
+    def test_file_objects(self):
+        """tests for __objects functionality"""
+        self.storage = FileStorage()
+        self.storage.save()
+        obj = self.storage.all()
+        self.assertIs(obj, self.storage._FileStorage__objects)
+
+    def test_all(self):
+        """testing the all module"""
+        self.storage = FileStorage()
+        self.storage.save()
+        obj = self.storage.all()
+        self.assertIsNotNone(obj)
+        self.assertEqual(type(obj), dict)
+        self.assertIs(obj, self.storage._FileStorage__objects)
+        self.assertEqual(dict, type(self.storage.all()))
+
+    def test_new(self):
+        """testing the new module"""
+        model = BaseModel()
+        model.save()
+        new_object = storage.all()
+        self.assertEqual(dict, type(new_object))
 
 if __name__ == "__main__":
     unittest.main()
